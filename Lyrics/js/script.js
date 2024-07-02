f02565b17b79fa2e42ba32a1d418eca165a3e085
iysrustandi <iyusrustandi2@gmail.com>
2024-07-02 14:54:51 +0700

    change slug to title tag on head

--- Lyrics/js/script.js
+++ Lyrics/js/script.js
@@ -7,19 +7,18 @@ $.get('../components/footer.html', function (data) {
   $('#footer-placeholder').replaceWith(data);
 });
 
-// ==========================Title to Slug=====================================
+// ==========================Title tag head to Slug=====================================
 function convertToSlug(text) {
   return text
     .toLowerCase()
-    .replace(/ /g, '-') // Ganti spasi dengan tanda hubung
-    .replace(/[^\w-]+/g, ''); // Hapus karakter non-alphanumeric kecuali tanda hubung
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '');
 }
 
 $(document).ready(function () {
-  const title = $('#title').text();
+  const title = $('title').text();
   const slug = convertToSlug(title);
 
-  // Ubah URL
   const currentPath = window.location.pathname.split('/').slice(0, -1).join('/');
   const newUrl = `${window.location.protocol}//${window.location.host}${currentPath}/${slug}`;
   window.history.replaceState({path: newUrl}, '', newUrl);
