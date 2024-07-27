8b9ae814297088b77361351354823ae9d9ea2832
iysrustandi <iyusrustandi2@gmail.com>
2024-07-27 21:58:30 +0700

    update style and songlist

--- js/script.js
+++ js/script.js
@@ -147,21 +147,21 @@ function FilterkeyWord_all_table() {
   }
 }
 
-// ==========================Title tag head to Slug=====================================
-function convertToSlug(text) {
-  return text
-    .toLowerCase()
-    .replace(/ /g, '-')
-    .replace(/[^\w-]+/g, '');
-}
+// // ==========================Title tag head to Slug=====================================
+// function convertToSlug(text) {
+//   return text
+//     .toLowerCase()
+//     .replace(/ /g, '-')
+//     .replace(/[^\w-]+/g, '');
+// }
 
-$(document).ready(function () {
-  const title = $('title').text();
-  const slug = convertToSlug(title);
+// $(document).ready(function () {
+//   const title = $('title').text();
+//   const slug = convertToSlug(title);
 
-  const currentPath = window.location.pathname.split('/').slice(0, -1).join('/');
-  const newUrl = `${window.location.protocol}//${window.location.host}${currentPath}/${slug}`;
-  window.history.replaceState({path: newUrl}, '', newUrl);
+//   const currentPath = window.location.pathname.split('/').slice(0, -1).join('/');
+//   const newUrl = `${window.location.protocol}//${window.location.host}${currentPath}/${slug}`;
+//   window.history.replaceState({path: newUrl}, '', newUrl);
 
-  console.log(newUrl);
-});
+//   console.log(newUrl);
+// });
